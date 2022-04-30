1b63302fd0b5d5fc97e91c60e838a99bc5a3cbe4
Issiah Banda <jarzarr@gmail.com>
2022-04-30 17:34:19 -0600

    Basic Pages

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import render_template, request
 	
 
 @app.route('/')
-def home():
-	return 'Hello World'
+def index():
+	return render_template('/index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+	username = request.values.get('user')
+	password = request.values.get('pass')
+	return username
